6b5ef199a9c0d08f16712e0aad26211de93676b7
mkamri <mail@mikamarie.com>
2020-02-13 11:40:00 +0900

    Change fetch url to support unit change

--- dist/js/weather.js
+++ dist/js/weather.js
@@ -3,12 +3,21 @@ class Weather {
     this.apiKey = '103e41e1f9fdd4e18a872b70f4a1c251';
     this.city = city;
     this.state = state;
+    this.units = 'imperial';
   };
 
   async getWeather(){
-    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.state}&appid=${this.apiKey}`);
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.state}&appid=${this.apiKey}&units=${units}`);
     const responseData = await response.json();
     return responseData;
+  }
+
+  changeUnits() {
+    if(this.units === 'imperial') {
+      this.units = 'metric'
+    } else if (this.units === 'metric') {
+      this.units = 'imperial';
+    }
   }
 
   changeLocation(newCity, newState) {
