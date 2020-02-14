eb97ad5c726ad511e8283d9a5fee3aa4bb5f6016
mkamri <mail@mikamarie.com>
2020-02-14 22:41:30 +0900

    Finish UI paint function and unit switch Add functionality to unit switch from imperial to metric and back, as well as finishing the UI paint funcitonFinish UI paint function and unit switch Add functionality to unit switch from imperial to metric and back, as well as finishing the UI paint funciton..

--- dist/js/app.js
+++ dist/js/app.js
@@ -4,9 +4,31 @@ const ui = new UI();
 // Init weather
 const weather = new Weather('Dayton', 'Ohio');
 
+// Set units to default (imperial)
+let units = 'imperial';
 
-weather.getWeather()
-  .then(results => {
-    ui.paint(results);
-  })
-  .catch(err => console.log(err));
+// Change units function
+
+document.getElementById('w-units').addEventListener('click', () => {
+    if(units === 'imperial') {
+      units = 'metric';
+    } else if (units === 'metric') {
+      units = 'imperial';
+    };
+    ui.unitsSwitch(units);
+    getWeather(units);
+    console.log(units);
+});
+
+
+// Get weather function
+function getWeather(units) {
+  weather.getWeather(units)
+    .then(results => {
+      ui.paint(results);
+    })
+    .catch(err => console.log(err));
+}
+
+// Call function
+getWeather(units);
--- dist/js/ui.js
+++ dist/js/ui.js
@@ -1,7 +1,6 @@
 class UI {
   constructor() {
     this.city = document.getElementById('w-city');
-    this.units = document.getElementById('w-units');
     this.icon = document.getElementById('w-icon');
     this.feelsLike = document.getElementById('w-feels-like');
     this.temperature = document.getElementById('w-temperature');
@@ -10,9 +9,24 @@ class UI {
   }
 
   paint(result){
-    this.city.textContent = weather.city;
-    this.units.textContent = 'F / C';
+    this.city.textContent = result.name;
+    this.unitsSwitch(units);
     this.icon.src = `http://openweathermap.org/img/wn/${result.weather[0].icon}@2x.png`;
-    this.feelsLike.textContent = result.main.feels_like.toFixed(1);
+    this.feelsLike.textContent = `${result.main.feels_like.toFixed(1)}\u00B0`;
+    this.temperature.textContent = `${result.main.temp.toFixed(1)}\u00B0`;
+    this.wind.textContent = `${result.wind.speed.toFixed(1)}`;
   };
+
+  unitsSwitch(units){
+    const imperialStyle = document.getElementById('w-imperial').style;
+    const metricStyle = document.getElementById('w-metric').style;
+
+    if (units === 'imperial'){
+      metricStyle.color = "#D3D3D3";
+      imperialStyle.color = "#000000";
+    } else if (units === 'metric'){
+      imperialStyle.color = "#D3D3D3";
+      metricStyle.color = "#000000";
+    }
+  }
 };
